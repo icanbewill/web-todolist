a58e43225e67dcf1312d83195426d2bd134a6c95
icanbewill <akimbiwil@gmail.com>
2023-03-04 01:53:17 +0100

    backend - Generation de la migration bdd

--- backend/database/migrations/2023_03_04_004038_create_tasks_table.php
+++ backend/database/migrations/2023_03_04_004038_create_tasks_table.php
@@ -16,7 +16,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->string("libelle");
-            $table->enum("state", ["a faire", "en cours", "fait", "en retard"]); // le statut en retard n'est pas tellement important, on peut le gérer comme traitement DANS LE MODELE
+            $table->enum("state", ["a faire", "en cours", "fait", "en retard"])->default("a faire"); // le statut en retard n'est pas tellement important, on peut le gérer comme traitement DANS LE MODELE
             $table->dateTime('deadline')->nullable();
             $table->timestamps();
         });
